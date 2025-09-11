7cabb04aeeac71d6d896ad5f1bfd616a17662f81
Renan Teixeira <rteixeira1997@gmail.com>
2025-09-10 22:50:04 -0300

    update

--- Python-POO/conta.py
+++ Python-POO/conta.py
@@ -3,3 +3,32 @@ class Conta:
         self.saldo=0
         self.numero=numero
         self.titular=titular
+
+    
+
+    @property
+    
+    def saldo(self):
+        return self._saldo
+
+    
+    @saldo.setter
+    def saldo(self, saldo):
+        if (saldo < 0):
+            print("saldo não pode ser negativo")
+        else:
+            self._saldo = saldo
+
+    
+    def saque(self, valor):
+        if (self.saldo>=valor):
+            self.saldo-=valor
+            print("Saque realizado com sucesso!")
+        else:
+            print("Saldo Insuficiente")
+
+    def deposita(self,valor):
+        self.saldo+=valor
+
+    def extrato(self):
+        print("Ciente: ",self.titular, "Saldo Atual: ", self._saldo)
--- Python-POO/main.py
+++ Python-POO/main.py
@@ -8,7 +8,11 @@ from clientes import cliente
 from conta import Conta
 
 c1= cliente("Ana", "123444-2222")
-conta=Conta(c1.nome, 6565,0)
+conta=Conta(c1.get_nome(), 6565,0)
+
+conta.deposita(1000)
+conta.saque(50)
+conta.extrato()
 
 print(c1)
 print(conta.titular, "Numero: ", conta.numero, "Seu Saldo: ", conta.saldo)
